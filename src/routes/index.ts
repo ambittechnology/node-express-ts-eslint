bd4f800003f47ed697b1d5246b5e09aa9b4d31cc
Frankfuurt <franciscogonzalez.inf@gmail.com>
2023-08-25 11:28:43 -0500

    add endpoint to test

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,15 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 // import middlewares from '../middlewares';
 import demo from './demo'
+import cors from 'cors'
 
 const { Router } = express
 const api = Router()
+
+const donation = {
+  user: 0,
+  amount: 0
+};
 
 // internal middleware
 // router.use(middlewares());
@@ -17,6 +23,49 @@ api.get('/', (req, res) => {
 api.get('/_health', (req, res) => {
   res.sendStatus(200)
 })
+
+// '/api/donate'
+api.post('/donate', (req, res) => {
+  const amount = req.body.amount || 0;
+
+  if (amount > 0) {
+    donation.amount += amount;
+    donation.user += 1;
+  }
+
+  return res.json({ message: 'Thank you 🙏 ' });
+})
+
+const SEND_INTERVAL = 2000;
+
+const writeEvent = (res: Response, sseId: string, data: string) => {
+  res.write(`id: ${sseId}\n`);
+  res.write(`data: ${data}\n\n`);
+};
+
+const sendEvent = (_req: Request, res: Response) => {
+  res.writeHead(200, {
+    'Cache-Control': 'no-cache',
+    Connection: 'keep-alive',
+    'Content-Type': 'text/event-stream',
+  });
+
+  const sseId = new Date().toDateString();
+
+  setInterval(() => {
+    writeEvent(res, sseId, JSON.stringify(donation));
+  }, SEND_INTERVAL);
+
+  writeEvent(res, sseId, JSON.stringify(donation));
+};
+
+api.get('/dashboard', (req: Request, res: Response) => {
+  if (req.headers.accept === 'text/event-stream') {
+    sendEvent(req, res);
+  } else {
+    res.json({ message: 'Ok' });
+  }
+});
 
 // set routes here
 api.use('/demo', demo)
